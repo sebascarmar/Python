550d24c0d1b68c36963165252b9ca9a491f3869b
sebacarmar <sebacarmar@gmail.com>
2023-06-03 01:38:05 -0300

    Agrega definición de funciones para ingreso y validación

--- TP2/multipliesGraficos.py
+++ TP2/multipliesGraficos.py
@@ -50,16 +50,7 @@ def figPlot(x, y, row, col, joinVec, numplot, typeGraf, xlim, ylim, xlabel, ylab
         pl.show()
     
 
-###########################################################################################
-
-
-row = int(input("Ingrese el número de filas: "))
-col = int(input("Ingrese el número de columnas: "))
-
-##################################################### INGRESO DE joinVec #################################################
-##########################################################################################################################
-ingreseOtraVez = 1
-while( ingreseOtraVez == 1 ):
+def ingresoDelJoinVec( ):
     # Paso 1: Solicitar al usuario los elementos de la tupla anidada
     ingresoAgrupaciones = input("\nIngrese las ubicaciones de los subplots separadas por comas, y las celdas unificadas separadas por espacios: ")
     
@@ -81,118 +72,81 @@ while( ingreseOtraVez == 1 ):
             tuplaInternaAgrupaciones.append(elementoInt)
     
     # Paso 5: Convertir la lista de tuplas internas en una tupla anidada
-    joinVec = tuple(tuplaInternaAgrupaciones)
-    
-    # Imprimir la tupla anidada resultante
-    #print("La tupla anidada ingresada es:", joinVec)
-    #print(len(joinVec))
-    
-    
-    ############################### VALIDACION DEL joinVec ###################################################33
-    ingreseOtraVez=0
-    cumpleCond1=True
-    cumpleCond2=True
-    cumpleCond3=True
+    joinVecAux = tuple(tuplaInternaAgrupaciones)
+    return joinVecAux
 
+
+def validaCantidadDeGraficasMenorACeldasTotales( joinVec, row, col, cumpleCond1Aux ):
     if( len(joinVec)>(row*col) ): #cantidad de elementos debe ser menor o igual al máximo (row*col)
         print("\nLa cantidad de gráficos requeridos supera la capacidad máxima de subplots ({}).".format(row*col),end=" ")
-        ingreseOtraVez=1
-        cumpleCond1=False
-  
-    if( cumpleCond1==True ): # Controla que ningún número de ubicación supere al máximo posible.
-       
-        for aux in joinVec:
-            
-            if( isinstance(aux, tuple) ): # Si es tupla.
-                
-                if( len(aux) <= 2 ): # si la agrupación de un gráfico es de solo dos elementos
-                    
-                    for aux2 in aux:
-                        if( aux2 > (row*col) ):
-                            print("\nValor de ubicación de suplot supera el máximo: {}.".format(aux),end=" ")
-                            ingreseOtraVez=1
-                            cumpleCond2=False
-                            break
+        cumpleCond1Aux=False
 
-                else:                # No permite que se ingresen agrupaciones de más de 2 elementos.
-                    print("\nLa agrupación se expresa solo con dos ubicaciones (max y min), no como: {}.".format(aux),end=" ")
-                    ingreseOtraVez=1
-                    cumpleCond2=False
-             
-            else:                         # Si es int. 
-                
-                if( aux > (row*col) ):
-                    print("\nValor de ubicación de suplot supera el máximo: {}.".format(aux),end=" ")
-                    ingreseOtraVez=1
-                    cumpleCond2=False
-                    break
-   
-    if( cumpleCond2==True ): # Controla que las ubicaciones no sean repetiadas.
-       
-        arrayAux = np.arange(1, row*col+1 , 1)
+    return cumpleCond1Aux
+
+
+def validaUbicacionNoSupereElMaximoPosible( joinVecm, row, col, cumpleCond2Aux ):
+    for aux in joinVec:
         
-        for aux in joinVec:
+        if( isinstance(aux, tuple) ): # Si es tupla.
             
-            if( isinstance(aux, tuple) ): # Si es tupla.
+            if( len(aux) <= 2 ): # si la agrupación de un gráfico es de solo dos elementos
                 
                 for aux2 in aux:
-                    contador=1
-                    for i in range(len(arrayAux)):
-                        if( aux2 == arrayAux[i] ):
-                            arrayAux[i]=0
-                            break # Sin este continua iterando y al final entra en la otra condición aunque se correcta la tupla
-                        elif( contador == len(arrayAux) ):
-                            print("\nUbicación repetida: {}.".format(aux),end=" ")
-                            ingreseOtraVez=1
-                            cumpleCond3=False
-                            break
-                        
-                        contador+=1
-             
-            else:                         # Si es int. 
+                    if( aux2 > (row*col) ):
+                        print("\nValor de ubicación de suplot supera el máximo: {}".format(aux),end=" ")
+                        cumpleCond2Aux=False
+                        break
+                
+            else:                # No permite que se ingresen agrupaciones de más de 2 elementos.
+                print("\nLa agrupación se expresa solo con dos ubicaciones (max y min), no como: {}".format(aux),end=" ")
+                cumpleCond2Aux=False
+         
+        else:                         # Si es int. 
+            
+            if( aux > (row*col) ):
+                print("\nValor de ubicación de suplot supera el máximo: {}".format(aux),end=" ")
+                cumpleCond2Aux=False
+                break
+
+    return cumpleCond2Aux
+
+
+def validaUbicacionesNoRepetidas( joinVec, row, col, cumpleCond3Aux ):
+   
+    arrayAux = np.arange(1, row*col+1 , 1)
+    
+    for aux in joinVec:
+        
+        if( isinstance(aux, tuple) ): # Si es tupla.
+            
+            for aux2 in aux:
                 contador=1
                 for i in range(len(arrayAux)):
-                    if( aux == arrayAux[i] ):
+                    if( aux2 == arrayAux[i] ):
                         arrayAux[i]=0
-                        break
+                        break # Sin este continua iterando y al final entra en la otra condición aunque sea correcta la tupla
                     elif( contador == len(arrayAux) ):
-                        print("\nUbicación repetida: {}.".format(aux),end=" ")
-                        ingreseOtraVez=1
-                        cumpleCond3=False
+                        print("\nUbicación repetida: {}".format(aux),end=" ")
+                        cumpleCond3Aux=False
                         break
                     
                     contador+=1
-            
-        
+         
+        else:                         # Si es int. 
+            contador=1
+            for i in range(len(arrayAux)):
+                if( aux == arrayAux[i] ):
+                    arrayAux[i]=0
+                    break
+                elif( contador == len(arrayAux) ):
+                    print("\nUbicación repetida: {}".format(aux),end=" ")
+                    cumpleCond3Aux=False
+                    break
+                
+                contador+=1
+    
+    return cumpleCond3Aux
 
-
-
-
-#joinVec=((1,2),3,(4,6),(7,9))
-
-
-##########################################################################################################################
-
-x = []
-for i in range(len(joinVec)): #Esto sería para 4 gráficos
-    x.append(np.arange(0.,5.,0.01))
-
-
-phase = 0.
-freq = 1.
-y=[]
-for i in range(len(x)): #Esto sería para 4 gráficos
-    auxY=np.sin(2.*np.pi*freq*x[0] + phase)
-    phase += np.pi/10.
-    freq += 1.
-    y.append(auxY)
-
-
-##########################################################################################################################
-
-numplot = int(input("\nIngrese el identificador numplot="))
-
-##########################################################################################################################
 
 def ingresoYValidacionTiposDeGraficos( lenJoinVec ):
     

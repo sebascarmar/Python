b2edcea7f489bf47bc70dab50262371e18ba77a8
sebacarmar <sebacarmar@gmail.com>
2024-01-31 12:42:15 -0300

    Define func. para usar como float a las cuantizaciones

--- TP6/src/fixedPoint/funciones.py
+++ TP6/src/fixedPoint/funciones.py
@@ -70,4 +70,16 @@ def eyediagram(data, n, offset, period):
         plt.plot(x, data[(i*span+xoff):((i+1)*span+xoff)],'b')       
     plt.grid(True)
     plt.xlim(xmin, xmax)
-    plt.show()
+    #plt.show()
+
+
+
+def arrayFix_to_arrayFloat(vect):
+    aux = []
+    for i in range(len(vect)):
+        aux.append(vect[i].fValue)
+
+    return aux
+
+
+

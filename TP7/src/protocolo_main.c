2150117f7ff96da38a5c551fee33abfda7d2c87e
sebacarmar <sebacarmar@gmail.com>
2024-02-23 16:58:30 -0300

    Versión funcional final

--- TP7/src/protocolo_main.c
+++ TP7/src/protocolo_main.c
@@ -29,62 +29,75 @@ XUartLite uart_module;
 
 int main()
 {
-    init_platform();
-    int Status;
-    XUartLite_Initialize(&uart_module, 0);
-    
-    GPO_Value=0x00000000;
-    GPO_Param=0x00000000;
-    
-    Status=XGpio_Initialize(&GpioInput, PORT_IN);
-    if(Status!=XST_SUCCESS)
+	int Status;
+	GPO_Value=0x00000000;
+	GPO_Param=0x00000000;
+	unsigned char data_in[3] = {'\0'};
+	u32           value;
+
+	init_platform();
+
+	XUartLite_Initialize(&uart_module, 0);
+
+
+	Status=XGpio_Initialize(&GpioInput, PORT_IN);
+	if(Status!=XST_SUCCESS)
         return XST_FAILURE;
-    
-    Status=XGpio_Initialize(&GpioOutput, PORT_OUT);
-    if(Status!=XST_SUCCESS)
-        return XST_FAILURE;
-    
-    XGpio_SetDataDirection(&GpioOutput, 1, 0x00000000);
-    XGpio_SetDataDirection(&GpioInput, 1, 0xFFFFFFFF);
-    
-    // Variables para lectura de GPIO 
-    u32           value     = 0;
-    unsigned char datEnviar = '\0';
-    // Variables para escritura en GPIO
-    unsigned char cabecera[1] = {'\0'};
-    unsigned char funcion[1]  = {'a'};
-    unsigned char data[2]     = {'\0'};
-    u32           conver      = 0;
-    
-    while(1)
-    {
-        if(cabecera[0]!=0x05) // Detecta byte de start
-        {
-            read(stdin,cabecera,1);
-        }
-        else if(funcion[0]!=0x00 || funcion[0]!=0xFF) // Lee byte de función
-        {
-            read(stdin,funcion,1);
-        }
-        else if(funcion[0]==0x00) // Si es función de lectura, lee los switches
-        {
-            XGpio_DiscreteWrite(&GpioOutput, 1, (u32) 0x00000000);
-            value     = XGpio_DiscreteRead(&GpioInput, 1);
-            datEnviar = (char) (value&(0x0000000F));
-            while(XUartLite_IsSending(&uart_module)){}
-            XUartLite_Send(&uart_module, &(datEnviar),1);
-        }
-        else                      // Si es función de escritura, escribe los leds
-        {
-            for(unsigned char i=0; i<=1; i++)
-                read(stdin,data[i],1);
-            
-            conver = (u32) ((data[1]<<8) | (data[0]));
-            XGpio_DiscreteWrite(&GpioOutput, 1, conver);
-        }
-        
-    }
-    
-    cleanup_platform();
-    return 0;
+
+	Status=XGpio_Initialize(&GpioOutput, PORT_OUT);
+	if(Status!=XST_SUCCESS)
+		return XST_FAILURE;
+
+	XGpio_SetDataDirection(&GpioOutput, 1, 0x00000000);
+	XGpio_SetDataDirection(&GpioInput, 1, 0xFFFFFFFF);
+
+
+	while(1)
+	{
+		if(data_in[0] != 0x05)
+		{
+			read(stdin,&data_in[0],1);
+		}
+		else
+		{
+			read(stdin,&data_in[1],1);
+			switch(data_in[1])
+			{
+		   	   case 0x55:
+				   XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000000);
+				   value = XGpio_DiscreteRead(&GpioInput, 1);
+				   data_in[2]=(char)(value&(0x0000000F));
+
+				   while(XUartLite_IsSending(&uart_module)){}
+				   XUartLite_Send(&uart_module, &(data_in[1]),1);
+
+				   while(XUartLite_IsSending(&uart_module)){}
+				   XUartLite_Send(&uart_module, &(data_in[2]),1);
+				   break;
+
+		   	   case 0xAA:
+		   		   read(stdin,&data_in[1],1);
+		   		   read(stdin,&data_in[2],1);
+
+		            value = (u32) ((data_in[1]<<8) | data_in[2])&0x0000FFFF;
+		            XGpio_DiscreteWrite(&GpioOutput, 1, value);
+
+		            data_in[1] = 0xAA;
+		            while(XUartLite_IsSending(&uart_module)){}
+		            XUartLite_Send(&uart_module, &(data_in[1]),1);
+
+		   	   default:
+		   		   while(XUartLite_IsSending(&uart_module)){}
+		   		   XUartLite_Send(&uart_module, &(data_in[1]),1);
+			} // fin switch funcion
+		   data_in[0]=!'\0';
+
+
+		}// fin del else
+
+	}// find del while
+	
+	cleanup_platform();
+	return 0;
 }
+

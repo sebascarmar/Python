06e31384d698bc8b27fac09f79ded4a197b07924
Andrea Taborda <andrea.taborda@mi.unc.edu.ar>
2024-06-24 18:18:13 -0300

    Cambia de nombre el archivo

--- TP8/src/protocolo_main.c
+++ TP8/src/protocolo_main.c
@@ -32,8 +32,18 @@ int main()
 	int Status;
 	GPO_Value=0x00000000;
 	GPO_Param=0x00000000;
-	unsigned char data_in[3] = {'\0'};
-	u32           value;
+	unsigned char data_in[3]  = {'\0'};
+	unsigned char data_mem[4] = {'\0'};
+	u32			  i_data    ;
+	unsigned char i_func    ;
+	u64           i_bit_err_I = 0x1111222233334444;
+    u64           i_bit_err_Q = 0x5555666677778888;
+	u64           i_bit_tot_I = 0xAAAABBBBCCCCDDDD;
+    u64           i_bit_tot_Q = 0xEEEEFFFFFFFFEEEE;
+	u32           i_read_data_from_mem = 0xF0F0F0F0;
+
+
+
 
 	init_platform();
 
@@ -54,52 +64,234 @@ int main()
 
 	while(1)
 	{
-		if(data_in[0] != 0x05)												// Si data_in [0] no es correcta
+		if(data_in[0] != 0xBB)								// Si data_in [0] no comienza con 0xBB (da error con 0x01)
 		{
-			read(stdin,&data_in[0],1);
+			read(stdin,&data_in[0],1);						// Espera hasta comenzar en 0xBB
 		}
-		else
+		else												 
 		{
-			read(stdin,&data_in[1],1);										// Se recibe data_in [1] 
+			read(stdin,&data_in[1],1);						// Se lee el segundo byte recibido
 			switch(data_in[1])
 			{
-				// Leer
-		   	   	case 0x55:
-				   XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000000);	// Todas las salidas en 0
-				   value = XGpio_DiscreteRead(&GpioInput, 1);				// Guarda todas las entradas en value
-				   data_in[2]=(char)(value&(0x0000000F));					// Se queda solo con los últimos 4 bits
+				// Reset
+		   	   	case 0x01:
+		   	   		i_func = data_in[1];
 
-				   while(XUartLite_IsSending(&uart_module)){}				// Envía data_in [1]: opcion
-				   XUartLite_Send(&uart_module, &(data_in[1]),1);
+					i_data = 0x00 & 0x7FFFFF;
+					write_GPIO(i_func, i_data);
+				
+					i_data = 0x01 & 0x7FFFFF;				// i_data para levantar nuevamente reset
+					write_GPIO(i_func, i_data);
 
-				   while(XUartLite_IsSending(&uart_module)){}				// Envía data_in [2]: estado switch
-				   XUartLite_Send(&uart_module, &(data_in[2]),1);
-				   break;
 
-				// Escrubir
-		   	   	case 0xAA:
-		   		   read(stdin,&data_in[1],1);									// Recibe data_in [1] y data_in [2] 
-		   		   read(stdin,&data_in[2],1);
-
-		            value = (u32) ((data_in[1]<<8) | data_in[2])&0x0000FFFF;	// Arma trama solo con leds 
-		            XGpio_DiscreteWrite(&GpioOutput, 1, value);					// Enciende leds
-
-		            data_in[1] = 0xAA;											// Actualiza valor
+					write_GPIO(0x00, i_data);									// Campo función puesto a 0
+				
+					// Comprobación de transmisión
 		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
-		            XUartLite_Send(&uart_module, &(data_in[1]),1);
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+				// Tx, Rx, Fase
+		   	   	case 0x02:
+		   	   	case 0x03:
+		   	   	case 0x04:
+		   			read(stdin,&data_in[2],1);									// Recibe el tercer byte
+
+		   			i_func = data_in[1];
+
+					i_data = data_in[2]& 0x7FFFFF;
+					write_GPIO(i_func, i_data);
+
+
+					write_GPIO(0x00, i_data);									// Campo función puesto a 0
+
+					// Comprobación de transmisión
+		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+				// Captura y envia BER
+				case 0x05:
+					i_func = data_in[1];
+
+					// Comprobación de transmisión
+		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+
+					i_data = 0x01 & 0x7FFFFF;
+					write_GPIO(i_func, i_data);
+
+					i_data = 0x00 & 0x7FFFFF;				  					// i_data para bajar bit capturar BER
+					// Obtiene las BER
+					get_BER(8, i_func, i_data)									// Hace un bucle de 8 veces para obtener la BER
+																				// Dentro del buche, las envía
+
+
+					write_GPIO(0x00, i_data);									// Campo función puesto a 0
+
+					// Comprobación de transmisión
+		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+
+				// Logueo memoria 
+				case 0x06:
+		   	   		i_func = data_in[1];
+
+
+					i_data = 0x01 & 0x7FFFFF;
+					write_GPIO(i_func, i_data);
+
+					i_data = 0x00 & 0x7FFFFF;									// i_data para bajar bit capturar y loguear memoria
+					write_GPIO(i_func, i_data);
+
+
+					write_GPIO(0x00, i_data);									// Campo función puesto a 0
+
+					// Comprobación de transmisión
+		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+
+				//Chequeo memoria
+				case 0x07:
+		   		
+		   			i_func = data_in[1];
+
+					// Comprobación de transmisión
+		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+
+					i_data = 0x00 & 0x7FFFFF;
+					write_GPIO(i_func, i_data);
+					
+					// Se sobreescribe i_data
+					i_data = (uint32_t)(XGpio_DiscreteRead(&GpioInput, 1));		// Compruebo si se lleno la memoria
+
+					while(XUartLite_IsSending(&uart_module)){}
+					XUartLite_Send(&uart_module, &(i_data),2);					// Envio bit
+
+
+					write_GPIO(0x00, i_data);									// Campo función puesto a 0
+
+					// Comprobación de transmisión
+		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+
+				// Lee memoria
+				case 0x08:
+					i_func = data_in[1];
+
+					// Comprobación de transmisión
+		            while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+		            XUartLite_Send(&uart_module, &(i_func),2);
+
+
+					i_data = 0x00 & 0x7FFFFF;
+
+					get_data(32769,  i_func);							// Hace un bucle de 32769 veces para obtener la memoria
+																		// Dentro del buche, las envía
+
+
+					write_GPIO(0x00, i_data);									// Campo función puesto a 0
+
+					// Comprobación de transmisión
+					while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+					XUartLite_Send(&uart_module, &(i_func),2);
 
 		   	   default:
-		   		   while(XUartLite_IsSending(&uart_module)){}
-		   		   XUartLite_Send(&uart_module, &(data_in[1]),1);
+			   		data_in[1] = 0xAA;
+					while(XUartLite_IsSending(&uart_module)){}
+					XUartLite_Send(&uart_module, &(data_in[1]),2);
+
 			} // fin switch funcion
 		   data_in[0]=!'\0';
+		   data_in[1]=!'\0';
 
 
 		}// fin del else
 
 	}// find del while
-	
+
 	cleanup_platform();
 	return 0;
 }
 
+
+
+void write_GPIO(unsigned char i_func, u32 i_data)
+{
+
+	u32           value;
+	unsigned char i_enable;
+
+	for (int i = 0; i < 3; ++i) {
+
+		i_enable = (i == 1) ? 0x01 : 0x00;
+
+		value = (u32) (i_func << 24) | (i_enable) << 23 | (i_data);
+		XGpio_DiscreteWrite(&GpioOutput, 1, value);
+	}
+
+/*
+	BLOQUE QUE ENVIA UNO DE LOS BYTES (106) ESCRITOS EN GPIO A LA UART
+	value = (u32) (i_func << 24) | (i_enable) << 23 | (i_data);
+	value_test[0] = (value >> 24)&0xFF;
+	value_test[1] = (value >> 16)&0xFF;
+	value_test[2] = (value >> 8)&0xFF;
+	value_test[3] = (value)&0xFF;
+
+	// Comprobación de transmisión al ciircuito DSP
+	while(XUartLite_IsSending(&uart_module)){}					// Envía data_in [1]: opcion
+	XUartLite_Send(&uart_module, &(value_test[1]),1);
+*/
+}
+
+// Función que envia los impulsos necesarios para obtener la BER y luego enviarla
+void get_BER(int i, unsigned char i_func, u32 i_data)
+{
+	uint32_t data_mem = 0;								
+
+	for (int j = 0; j < i; ++j)
+	{
+		write_GPIO(i_func, i_data);									// Impulos para obtener 32 bits
+		data_mem = (uint32_t)(XGpio_DiscreteRead(&GpioInput, 1));	// Leo los bits
+
+		send_data(data_mem)						        			// Envio 32 bits
+	}
+}
+
+
+// Función que envia los impulsos necesarios para leer la memoria y luego enviarla
+void get_data(unsigned short int i, unsigned char i_func)
+{
+	uint32_t data_mem = 0;								
+
+	for (unsigned short int j = 0; j < i; ++j)
+	{
+		write_GPIO(i_func, j);										// Impulos para obtener 32 bits
+		data_mem = (uint32_t)(XGpio_DiscreteRead(&GpioInput, 1));	// Leo los bits
+
+		send_data(data_mem)						        			// Envio 32 bits
+	}
+}
+
+
+// Función que separa de a 1 byte y los envía
+void send_data(uint32_t data_ber)
+{
+	uint32_t byte_ber [4] = {'\0'};
+
+	byte_ber[0] = (data_ber >> 24)&0xFF;							// Separo en bytes
+	byte_ber[1] = (data_ber >> 16)&0xFF;
+	byte_ber[2] = (data_ber >> 8 )&0xFF;
+	byte_ber[3] = (data_ber      )&0xFF;
+
+	for(int i = 0; i < 4; ++i)
+	{
+		while(XUartLite_IsSending(&uart_module)){}					// Envía 1 byte
+		XUartLite_Send(&uart_module, &(byte_ber[i]),2);
+	}
+
+}
